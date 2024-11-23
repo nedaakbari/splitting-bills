85fc0a0128f9e2cdfae514977336a62ed24cfb9c
nedaakbari <nedaakbari555@gmail.com>
2024-11-23 21:30:11 +0330

    init project

---
+++ src/main/java/ir/splitwise/splitbills/SplittingBillsApplication.java
@@ -0,0 +1,13 @@
+package ir.splitwise.splitbills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplittingBillsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplittingBillsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/splitwise/splitbills/SplittingBillsApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.splitwise.splitbills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplittingBillsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
