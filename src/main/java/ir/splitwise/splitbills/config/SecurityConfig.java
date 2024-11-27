9d683ca94becf707778ba92055af6f449ea620f1
nedaakbari <nedaakbari555@gmail.com>
2024-11-28 03:14:14 +0330

    reversion pom.xml

--- src/main/java/ir/splitwise/splitbills/config/SecurityConfig.java
+++ src/main/java/ir/splitwise/splitbills/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -21,7 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf().disable()
+                .csrf(CsrfConfigurer::disable)
                 .authorizeHttpRequests(
                         authRequest -> authRequest.requestMatchers(
                                         "/auth/**",
